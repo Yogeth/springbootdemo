a4f79c3f5f21c9604141d679337236d8cd385aec
Yogeth <yogethcr@gmail.com>
2025-12-02 21:16:55 +0530

    update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-// Iam Yogeth ✨✨
+// Iam wayne ✨✨
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
