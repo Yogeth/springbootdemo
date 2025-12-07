776520ecb65d796fdb2e34570b8d3c004d1cc047
Yogeth <yogethcr@gmail.com>
2025-12-08 05:10:14 +0530

    update Student.java

--- demo/src/main/java/com/example/demo/Student.java
+++ demo/src/main/java/com/example/demo/Student.java
@@ -11,10 +11,18 @@ public class Student{
 //  @Autowired
   private Pen pen;
   //setter injection
-  @Autowired
+ /* @Autowired
   public void setPen(Pen pen){
+    System.out.println("this is setter injection");
+    this.pen=pen;
+  } */
+  //constructor injection
+  @Autowired
+  Student(Pen pen){
+    System.out.println("This is constructor injecton!!");
     this.pen=pen;
   }
+  
   public void note(){
     System.out.println("im note book");
     pen.write();
