66a6e31e445c2e3977ff25d5a98f57955986cf9c
Yogeth <yogethcr@gmail.com>
2025-12-07 06:39:40 +0530

    update DemoApplication.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,5 @@
 package com.example.demo;
-
+// applcon
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
