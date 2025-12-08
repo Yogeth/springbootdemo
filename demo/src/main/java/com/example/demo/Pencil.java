8cb44cf1cb100350e24b47a6574471b26fb3c920
Yogeth <yogethcr@gmail.com>
2025-12-08 06:03:10 +0530

    update Pencil.java

--- demo/src/main/java/com/example/demo/Pencil.java
+++ demo/src/main/java/com/example/demo/Pencil.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class Pencil implements Writer{
   public void writing(){
-    System.out.println("writing using pencil ")
+    System.out.println("writing using pencil ");
   }
 }
