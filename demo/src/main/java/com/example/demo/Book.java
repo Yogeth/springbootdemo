acdec2b3d6f7870eb7f3e6f1fd9a629189614d93
Yogeth <yogethcr@gmail.com>
2025-12-08 06:14:28 +0530

    update Book.java

--- demo/src/main/java/com/example/demo/Book.java
+++ demo/src/main/java/com/example/demo/Book.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Component;
+@Component
 public class Book{
   @Autowired
   private Writer writer;
