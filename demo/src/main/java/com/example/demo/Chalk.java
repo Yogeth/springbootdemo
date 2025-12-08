1943b525a331cddc84dbaf9f97fc63b19335cfc0
Yogeth <yogethcr@gmail.com>
2025-12-08 06:09:32 +0530

    update Chalk.java

--- demo/src/main/java/com/example/demo/Chalk.java
+++ demo/src/main/java/com/example/demo/Chalk.java
@@ -2,7 +2,7 @@ package com.example.demo;
 import org.springframework.stereotype.Component;
 
 @Component
-pubilc class Chalk implements Writer{
+public class Chalk implements Writer{
   public void writing(){
     System.out.println("writing using chalk piece !!");
   }
