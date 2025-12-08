b6b16f5ce072e1249b6858807cd38625eff32277
Yogeth <yogethcr@gmail.com>
2025-12-08 06:15:36 +0530

    update

--- demo/src/main/java/com/example/demo/Book.java
+++ demo/src/main/java/com/example/demo/Book.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 @Component
 public class Book{
   @Autowired
--- demo/src/main/java/com/example/demo/Chalk.java
+++ demo/src/main/java/com/example/demo/Chalk.java
@@ -2,6 +2,7 @@ package com.example.demo;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Chalk implements Writer{
   public void writing(){
     System.out.println("writing using chalk piece !!");
